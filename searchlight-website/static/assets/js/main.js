7d2029ffa0ffaa7c26c9fb998b093d8eae14acc9
Omkar Waingankar <omkar.waingankar@berkeley.edu>
2018-04-12 12:16:15 -0700

    get requests working, new filters added

--- searchlight-website/static/assets/js/main.js
+++ searchlight-website/static/assets/js/main.js
@@ -8,9 +8,11 @@
 $('#filter-btn').click(function() {
 	//Reload the page to display the new data.
 	//You could optionally work with Ajax
-	window.location.href="/query?name=" + $("#speaker").val() + "&year=" + $("#year").val()
+	window.location.href="/query?surname=" + $("#surname").val() + "&firstname=" + $("#firstname").val() +
+	"&district=" + $("#district").val() + "&state=" + $("#state").val() + "&party=" + $("#party").val() + "&type=" + $("#type").val() +
+	"&month=" + $("#month").val() + "&day=" + $("#day").val() + "&year=" + $("#year").val()
 });
 
 $('#download-btn').click(function() {
-	window.location.href="/query?format=csv&name=" + $("#speaker").val() + "&year=" + $("#year").val()
+	window.location.href="/query?format=csv&surname=" + $("#speaker").val() + "&year=" + $("#year").val()
 });
