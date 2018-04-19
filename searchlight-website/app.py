8d0b5ecd494a7d3fe3de53d2d0c27e0e30f6e199
Nalin Chopra <nalin.chopra@berkeley.edu>
2018-04-18 23:21:54 -0700

    modifying sql query logic to search through database

--- searchlight-website/app.py
+++ searchlight-website/app.py
@@ -90,16 +90,13 @@ def speakers():
 						inner join speeches on speakers.speaker_id = speeches.speaker_id \
 						%s %s;"
 
-	where_clause = ""
+	where_clause = "where "
+	where_array = []
+	condition_tuple = []
 	if speaker_surname or speaker_firstname or district_query or state_query or party_query or type_query or month or day or year:
-		where_clause = "where "
-		where_array = []
-		condition_tuple = []
-
 		if speaker_surname:
 			where_array.append("last_name like ? ")
 			condition_tuple.append("%" + str(speaker_surname) + "%")
-			+ str(speaker_surname))
 		if speaker_firstname:
 			where_array.append("first_name like ? ")
 			condition_tuple.append("%" + str(speaker_firstname_raw) + "%")
@@ -107,71 +104,72 @@ def speakers():
 			where_array.append("type = ? ")
 			condition_tuple.append(str(type_query))
 		if party_query:
-			where_array.append("party = " + str(party_query))
-			condition_tuple.append()
+			where_array.append("party = ? ")
+			condition_tuple.append(str(party_query))
 		if state_query:
-			where_array.append("state = " + str(state_query))
-		if district_query:
-			where_array.append("district = " + str(district_query)
-		if month:
-			where_array.append("month = " + str(month))
+			where_array.append("state = ? ")
+			condition_tuple.append(str(state_query))
 		if day:
-			where_array.append("day = " + str(day))
+			where_array.append("day = ? ")
+			condition_tuple.append(str(day))
 		if year:
-			where_array.append("year = " + str(year))
+			where_array.append("year = ? ")
+			condition_tuple.append(str(year))
+		if month:
+			where_array.append("month = ? ")
+			condition_tuple.append(str(month))
+		if district_query:
+			where_array.append("district = ? ")
+			condition_tuple.append(str(district_query))
 
-	where_clause = "where " +
-	start_var = true;
-	for query in where_array:
-		if start_var:
-			where_clause += query
-			start_var = false
-		else:
-			where_clause += " and " + query
-	limit_statement = "limit 20" if format_ != "csv" else ""
-
-
-"""		if speaker:
-			where_clause += " speaker.surname = ? " if speaker else ""
-		if year and speaker:
-			where_clause += " and "
-		if year:
-			where_clause += " hearing.date like ? " if len(year)>2 else ""
-	limit_statement = "limit 20" if format_ != "csv" else ""
-"""
-
+		condition_tuple = tuple(condition_tuple)
+	limit_statement = "LIMIT 20" if format_ != "csv" else ""
 	all_records_query = all_records_query % (where_clause, limit_statement)
 	print(all_records_query)
 
-	cursor
-
-	if speaker and year:
-		cursor.execute(all_records_query ,(speaker.lower(), "%"+ year))
-	elif speaker:
-		cursor.execute(all_records_query ,(speaker.lower(),))
-	elif year:
-		cursor.execute(all_records_query ,("%"+ year,))
-	else:
-		cursor.execute(all_records_query)
+	cursor.execute(all_records_query, condition_tuple)
 	records = cursor.fetchall()
-
-	#Query to count the number of records
-	count_query =  "SELECT count(*) as count FROM hearing inner join speech on \
-				speech.hearing_id = hearing.hearing_id inner join speaker \
-				on speaker.speech_id = speech.speech_id %s;"
-	count_query = count_query % (where_clause)
-	if speaker and year:
-		cursor.execute(count_query, (speaker.lower(), "%"+ year))
-	elif year:
-		cursor.execute(count_query, ("%"+ year,))
-	elif speaker:
-		cursor.execute(count_query, (speaker.lower()))
-	else:
-		cursor.execute(count_query)
-	#There's a lot of if else going on here but I will send a better solution for you guys to work with
-	no_of_records = cursor.fetchall()
 	connection.close()
 
+
+	#if speaker:
+	#	where_clause += " speaker.surname = ? " if speaker else ""
+	#if year and speaker:
+	#	where_clause += " and "
+	#if year:
+	#	where_clause += " hearing.date like ? " if len(year)>2 else ""
+
+
+
+	# if speaker and year:
+	# 	cursor.execute(all_records_query ,(speaker.lower(), "%"+ year))
+	# elif speaker:
+	# 	cursor.execute(all_records_query ,(speaker.lower(),))
+	# elif year:
+	# 	cursor.execute(all_records_query ,("%"+ year,))
+	# else:
+	# 	cursor.execute(all_records_query)
+	# records = cursor.fetchall()
+
+	# #Query to count the number of records
+	# count_query =  "SELECT count(*) as count FROM hearing inner join speech on \
+	# 			speech.hearing_id = hearing.hearing_id inner join speaker \
+	# 			on speaker.speech_id = speech.speech_id %s;"
+	# count_query = count_query % (where_clause)
+	# if speaker and year:
+	# 	cursor.execute(count_query, (speaker.lower(), "%"+ year))
+	# elif year:
+	# 	cursor.execute(count_query, ("%"+ year,))
+	# elif speaker:
+	# 	cursor.execute(count_query, (speaker.lower()))
+	# else:
+	# 	cursor.execute(count_query)
+
+
+
+	#There's a lot of if else going on here but I will send a better solution for you guys to work with
+	#no_of_records = cursor.fetchall()
+	#connection.close()
 	#Send the information back to the view
 	#if the user specified csv send the data as a file for download else visualize the data on the web page
 	if format_ == "csv":
